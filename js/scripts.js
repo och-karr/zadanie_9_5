a6ea9234363d4dc58b22327bf17bfcdab27a40c0
karolinneczka <okaa93@gmail.com>
2018-04-25 17:56:10 +0200

    Add basic files and add a simple program in js file - DOM - first version unfinished

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var withMenuItemsClass = document.getElementsByClassName('button');
+
+alert(withMenuItemsClass); // tu się wyswietla [object HTMLCollection]
+console.log(withMenuItemsClass.length); //tu się wyswietla 3
+
+// Stwórz pętlę for, która wykona się tyle razy, ile jest elementów na stronie - przyda ci się do tego właściwość length.
+
+// Nie wiem jak się zabrać za to polecenie. Nie wiem w jaki sposb pobrać treść z buttonow - nie bylo takiego polecenia.. 
